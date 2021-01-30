8e734a5b0e51fb3c94acece486773dfa3712a4a5
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-01-29 21:30:55 -0300

    Criação dos componentes Soma, Media e Sorteio

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,18 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Card from './components/Card';
+import Intervalo from './components/Intervalo';
+import Media from './components/Media';
+import Soma from './components/Soma';
+import Sorteio from './components/Sorteio';
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>Exercício React-Redux</h1>
       <div className="linha">
-        <Card title="Card 1" red>x</Card>
-        <Card title="Card 2" green>x</Card>
+        <Intervalo></Intervalo>
       </div>
       <div className="linha">
-        <Card title="Card 3" blue>y</Card>
-        <Card title="Card 4" purple>y</Card>
+        <Media/>
+        <Soma/>
+        <Sorteio/>
       </div>
     </div>
   );
---
+++ redux-simples/src/components/Sorteio.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './Card';
+
+export default props => {
+    return (
+        <Card title="Sorte de um número" purple>
+            <div>
+                <span>
+                    <span>Resultado: </span>
+                    <strong>8</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
