1ac0ced6e93d78bc0e68ef9d9f12f935250eb369
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-02-01 21:15:36 -0300

    Invocando Action Creator

--- redux-simples/src/components/Intervalo.jsx
+++ redux-simples/src/components/Intervalo.jsx
@@ -3,20 +3,22 @@ import React from 'react'
 import Card from './Card';
 import { connect } from 'react-redux';
 
+import {alterarNumeroMinimo} from '../store/actions/numeros'
+import {alterarNumeroMaximo} from '../store/actions/numeros'
  const Intervalo = props => {
 
     const {max, min} = props;
-    
+
     return (
         <Card title="Intervalo de Números" red>
             <div className="Intervalo">
                 <span>
                     <strong>Mínimo: </strong>
-                    <input type="number" value={min} readOnly />
+                    <input type="number" value={min} onChange={e => props.alterarMinimo(+e.target.value)} />
                 </span>
                 <span>
-                    <strong>Mínimo :</strong>
-                    <input type="number" value={max} readOnly />
+                    <strong>Máximo :</strong>
+                    <input type="number" value={max} onChange={e=> props.alterarMaximo(+e.target.value)} />
                 </span>
             </div>
         </Card>
@@ -31,4 +33,19 @@ function mapStateToProps(state){
     }
 }
 
-export default connect(mapStateToProps)(Intervalo)
+function mapDispatchToProps(dispatch) {
+    return {
+        alterarMinimo(novoNumero) {
+            /* action creator => action */
+            const action = alterarNumeroMinimo(novoNumero)
+            dispatch(action)
+        },
+
+        alterarMaximo(novoNumero) {
+            const action = alterarNumeroMaximo(novoNumero)
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Intervalo)
--- redux-simples/src/store/storeConfig.js
+++ redux-simples/src/store/storeConfig.js
@@ -2,12 +2,34 @@ import {createStore, combineReducers} from 'redux'
 
 const reducers = combineReducers({
     numeros: function(state, action) {
-        console.log(state, ' ', action)
-        return {
-            min: 7, 
-            max: 31
+        
+        switch (action.type) {
+            case 'NUM_MIN_ALTERADO':
+                return {
+                    ...state,
+                    min: action.payload
+                }
+            case 'NUM_MAX_ALTERADO': 
+                return   {
+                    ...state,
+                    max: action.payload
+                }          
+            default:
+                return {
+                    min: 7,
+                    max: 31
+                };
         }
+    },
+    nomes: function(state, action) {
+        console.log('Reducers nomes');
+        return [
+            'Ana',
+            'Bia',
+            'Carlos'
+        ]
     }
+
 })
 
 function storeConfig() {
