a1a8d83607724bd7e0c9ae713e34992c414f554e
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-02-01 21:27:24 -0300

    Refatorando Redux

--- redux-simples/src/store/storeConfig.js
+++ redux-simples/src/store/storeConfig.js
@@ -1,35 +1,9 @@
 import {createStore, combineReducers} from 'redux'
+import numerosReducers from './reducers/numeros'
+
 
 const reducers = combineReducers({
-    numeros: function(state, action) {
-        
-        switch (action.type) {
-            case 'NUM_MIN_ALTERADO':
-                return {
-                    ...state,
-                    min: action.payload
-                }
-            case 'NUM_MAX_ALTERADO': 
-                return   {
-                    ...state,
-                    max: action.payload
-                }          
-            default:
-                return {
-                    min: 7,
-                    max: 31
-                };
-        }
-    },
-    nomes: function(state, action) {
-        console.log('Reducers nomes');
-        return [
-            'Ana',
-            'Bia',
-            'Carlos'
-        ]
-    }
-
+    numeros: numerosReducers,
 })
 
 function storeConfig() {
